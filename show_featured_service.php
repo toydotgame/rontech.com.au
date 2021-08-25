bf7fe6f42afbe34079429344ac47b1883d2c6c7e
toydotgame <toydotgame@gmail.com>
2021-08-25 18:05:14 +1000

    Removed all files, starting from scratch

--- show_featured_service.php
+++
@@ -1,8 +0,0 @@
-<table border="0" cellspacing="0" cellpadding="0">
-  <tr>
-	<td width="52" align="left" valign="top" style="padding-right:10px; padding-left:10px;"><a href="rontech_security_designs.php" title="Security Design Professional"><img src="images/ajx_dv436026_n.gif" alt="Security Design Professional" border="0" title="Security Design Professional"/></a></td>
-	<td width="183" align="left" valign="top" ><h2>Security Design Professional</h2>
-	  <span class="h2_txt_black">Rontech Security provides security design, installation, service and monitoring to small and large p...</span><br />
-	<div align="right"><a href="rontech_security_designs.php" class="send_enquiry" title="More">more >></a></div></td>
-  </tr>
-</table>
